2033cd735a3dc8f63fe7cb9d7f19851292e854ca
nigel <9341256+nigel139@user.noreply.gitee.com>
2025-09-29 14:49:44 +0000

    pages
    Signed-off-by: nigel <9341256+nigel139@user.noreply.gitee.com>

---
+++ webAdmin/types/wangeditor-editor-for-vue.d.ts
@@ -0,0 +1,7 @@
+// types/wangeditor-editor-for-vue.d.ts
+declare module '@wangeditor/editor-for-vue' {
+  import { DefineComponent } from 'vue'
+  const Editor: DefineComponent
+  const Toolbar: DefineComponent
+  export { Editor, Toolbar }
+}
