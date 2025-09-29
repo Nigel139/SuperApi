7068715766182e8a807ffd736a6f26693a759e15
nigel <9341256+nigel139@user.noreply.gitee.com>
2025-09-29 14:48:54 +0000

    pages
    Signed-off-by: nigel <9341256+nigel139@user.noreply.gitee.com>

---
+++ webAdmin/typeModule/AppModule.ts
@@ -0,0 +1,11 @@
+export interface Theme {
+  sidebarStyle: string
+  headerStyle: string
+  primaryColor: string
+  uniqueOpened: boolean
+  isLogo: boolean
+  isBreadcrumb: boolean
+  isTabsView: boolean
+  isTabsCache: boolean
+  tabsStyle: string
+}
